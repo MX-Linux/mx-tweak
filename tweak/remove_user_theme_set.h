49b8c870ab8648e4a404d206349ab56114f8ff44
TheNiteCoder <stallionsamueld@gmail.com>
2019-08-07 18:08:35 -0400

    Adding a user theme set removal button

---
+++ tweak/remove_user_theme_set.h
@@ -0,0 +1,39 @@
+#ifndef REMOVE_USER_THEME_SET_H
+#define REMOVE_USER_THEME_SET_H
+
+#include <QDialog>
+
+namespace Ui {
+class remove_user_theme_set;
+}
+
+class QComboBox;
+
+struct ExecResult
+{
+    QString output;
+    int exitCode;
+};
+
+class remove_user_theme_set : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit remove_user_theme_set(QWidget *parent = 0);
+    ~remove_user_theme_set();
+
+    QComboBox *themeSelector();
+
+    void setupThemeSelector();
+
+    ExecResult runCmd(QString cmd);
+
+    QString getFilename(QString name);
+
+private:
+    Ui::remove_user_theme_set *ui;
+    QHash<QString, QString> theme_info;
+};
+
+#endif // REMOVE_USER_THEME_SET_H
