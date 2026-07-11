b9b05eeb594747faff8fe6f5453247a015a714fd
Dolphin Oracle <dolphinoracle@gmail.com>
2026-07-11 09:25:32 -0400

    throw away arguements to passwordless root checks

--- lib/mx-tweak-check.sh
+++ lib/mx-tweak-check.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -n "$@" ]; then
+    echo "no arguements accepted"
+	exit 2
+fi
 test=$(passwd --status root|cut -d' ' -f2)
 if [ "$test" = "NP" ] || [ "$test" = "L" ]; then
 	echo "NP"
--- lib/mx-tweak-rootcheck.sh
+++ lib/mx-tweak-rootcheck.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -n "$@" ]; then
+    echo "no arguements accepted"
+	exit 2
+fi
 if [ -f /etc/polkit-1/rules.d/10-default-mx.rules ]; then
 	exit 0
 else
