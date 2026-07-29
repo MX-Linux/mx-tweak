5b271df46490612b6b1dbc2ac9b758e389ac315a
Dolphin Oracle <dolphinoracle@gmail.com>
2026-07-28 21:05:05 -0400

    dark mode checkbox fluxbox (review item 8)

--- tweak_theme.cpp
+++ tweak_theme.cpp
@@ -895,7 +895,7 @@ void TweakTheme::checkThemeGTKDarkMode_checkStateChanged(Qt::CheckState state) c
         if (runCmd(u"grep gtk-application-prefer-dark-theme $HOME/.config/gtk-3.0/settings.ini"_s).exitCode == 0) {
             runCmd(u"sed -i 's/gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=false/' $HOME/.config/gtk-3.0/settings.ini"_s);
         } else {
-            runCmd(u"echo gtk-application-prefer-dark-theme=false/' >> $HOME/.config/gtk-3.0/settings.ini"_s);
+            runCmd(u"echo gtk-application-prefer-dark-theme=false >> $HOME/.config/gtk-3.0/settings.ini"_s);
         }
     }
 }
